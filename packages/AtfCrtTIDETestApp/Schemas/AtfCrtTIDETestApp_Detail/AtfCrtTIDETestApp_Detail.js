14026c31f425beffedad5e7f614398dc26ce8c4b
e.zinchenko <e.zinchenko@creatio.com>
2025-10-12 21:51:11 +0300

    ыфвыф

---
+++ packages/AtfCrtTIDETestApp/Schemas/AtfCrtTIDETestApp_Detail/AtfCrtTIDETestApp_Detail.js
@@ -0,0 +1,8 @@
+define('AtfCrtTIDETestApp_Detail', [], function () {
+	return {
+		entitySchemaName: 'AtfCrtTIDETestApp',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ packages/AtfCrtTIDETestApp/Schemas/AtfCrtTIDETestApp_ListPage/AtfCrtTIDETestApp_ListPage.js
@@ -0,0 +1,237 @@
+define("AtfCrtTIDETestApp_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "MenuItem_ImportFromExcel",
+				"values": {
+					"clicked": {
+						"request": "crt.ImportDataRequest",
+						"params": {
+							"entitySchemaName": "AtfCrtTIDETestApp"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "FolderTree",
+				"values": {
+					"rootSchemaName": "AtfCrtTIDETestApp"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DataTable",
+				"values": {
+					"columns": [
+						{
+							"id": "f252f581-0ccf-44ac-b7c9-c00df2ad9919",
+							"code": "PDS_AtfName",
+							"caption": "#ResourceString(PDS_AtfName)#",
+							"dataValueType": 1
+						},
+						{
+							"id": "c8689d78-80ba-4e71-8cf2-fa478e3be5bc",
+							"code": "PDS_CreatedOn",
+							"caption": "#ResourceString(PDS_CreatedOn)#",
+							"dataValueType": 7
+						},
+						{
+							"id": "fd4b3485-a46e-4219-b775-adef1210fe51",
+							"code": "PDS_CreatedBy",
+							"caption": "#ResourceString(PDS_CreatedBy)#",
+							"dataValueType": 10
+						}
+					]
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Dashboards",
+				"values": {
+					"_designOptions": {
+						"entitySchemaName": "AtfCrtTIDETestApp",
+						"dependencies": [
+							{
+								"attributePath": "Id",
+								"relationPath": "PDS.Id"
+							}
+						],
+						"filters": []
+					}
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "ChartWidget_m5lzlml",
+				"values": {
+					"type": "crt.ChartWidget",
+					"config": {
+						"title": "#ResourceString(ChartWidget_m5lzlml_title)#",
+						"color": "dark-blue",
+						"theme": "without-fill",
+						"scales": {
+							"stacked": false,
+							"xAxis": {
+								"name": "",
+								"formatting": {
+									"type": "string",
+									"maxLinesCount": 2,
+									"maxLineLength": 10
+								}
+							},
+							"yAxis": {
+								"name": "",
+								"formatting": {
+									"type": "number",
+									"thousandAbbreviation": {
+										"enabled": true
+									}
+								}
+							}
+						},
+						"series": [
+							{
+								"color": "burnt-coral",
+								"type": "bar",
+								"label": "#ResourceString(ChartWidget_m5lzlml_series_0)#",
+								"legend": {
+									"enabled": false
+								},
+								"data": {
+									"providing": {
+										"attribute": "ChartWidget_m5lzlml_SeriesData_zxkgmyi",
+										"schemaName": "Account",
+										"filters": {
+											"filter": {
+												"items": {
+													"columnIsNotNullFilter": {
+														"comparisonType": 2,
+														"filterType": 2,
+														"isEnabled": true,
+														"isNull": false,
+														"trimDateTimeParameterToDate": false,
+														"leftExpression": {
+															"expressionType": 0,
+															"columnPath": "Address"
+														}
+													}
+												},
+												"logicalOperation": 0,
+												"isEnabled": true,
+												"filterType": 6,
+												"rootSchemaName": "Account"
+											},
+											"filterAttributes": []
+										},
+										"aggregation": {
+											"column": {
+												"orderDirection": 0,
+												"orderPosition": -1,
+												"isVisible": true,
+												"expression": {
+													"expressionType": 1,
+													"functionArgument": {
+														"expressionType": 0,
+														"columnPath": "Id"
+													},
+													"functionType": 2,
+													"aggregationType": 1,
+													"aggregationEvalType": 2
+												}
+											}
+										},
+										"dependencies": [],
+										"rowCount": 50,
+										"grouping": {
+											"type": "by-value",
+											"column": {
+												"orderDirection": 0,
+												"orderPosition": -1,
+												"isVisible": true,
+												"expression": {
+													"expressionType": 0,
+													"columnPath": "Address"
+												}
+											}
+										}
+									},
+									"formatting": {
+										"type": "number",
+										"decimalSeparator": ".",
+										"decimalPrecision": 0,
+										"thousandSeparator": ","
+									}
+								},
+								"dataLabel": {
+									"display": true
+								}
+							}
+						],
+						"seriesOrder": {
+							"type": "by-grouping-value",
+							"direction": 1
+						},
+						"layout": {}
+					},
+					"sectionBindingColumnRecordId": "$Id"
+				},
+				"parentName": "LeftFilterContainerInner",
+				"propertyName": "items",
+				"index": 3
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"viewModelConfig",
+					"attributes"
+				],
+				"values": {
+					"PDS_AtfName": {
+						"modelConfig": {
+							"path": "PDS.AtfName"
+						}
+					},
+					"PDS_CreatedOn": {
+						"modelConfig": {
+							"path": "PDS.CreatedOn"
+						}
+					},
+					"PDS_CreatedBy": {
+						"modelConfig": {
+							"path": "PDS.CreatedBy"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "remove",
+				"path": [],
+				"properties": [
+					"dependencies"
+				]
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources",
+					"PDS",
+					"config"
+				],
+				"values": {
+					"entitySchemaName": "AtfCrtTIDETestApp"
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
